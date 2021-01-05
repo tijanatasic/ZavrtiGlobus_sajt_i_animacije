43dc33575dc3518e8d1edcdfb7d88a4dba6dec57
Tijana Tasic <tijanatasic3@gmail.com>
2021-01-06 00:20:45 +0100

    First commit

---
+++ drzave/norveska/javascript1.js
@@ -0,0 +1,14 @@
+// var animation1=bodymovin.loadAnimation({
+//     container: document.getElementById('animacija'),
+//     renderer: 'svg',
+//     loop: false,
+//     autoplay: false,
+//     path: 'data.json'
+// });
+
+// function pustiAnimaciju(){
+//     animation1.play();
+// }
+
+
+ 
---
+++ javascript.js
@@ -0,0 +1,8 @@
+// var animation=bodymovin.loadAnimation({
+//     container: document.getElementById('animacija'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'data.json'
+// });
+
